a90bd6c3eb35f814ec840c09e3e7868c07df1cc1
Nik PC <nik.vozelj@hotmail.com>
2024-07-19 21:12:15 +0200

    Changed navigation item names

--- src/traceNavigation.ts
+++ src/traceNavigation.ts
@@ -7,9 +7,9 @@ export class NavigationTreeDataProvider implements TreeDataProvider<TracetoolTre
 
     constructor() {
         this.items = [
-            new TracetoolTreeItem('Transaction', 'Start transaction'),
-            new TracetoolTreeItem('Policy', 'Applying policy'),
-            new TracetoolTreeItem('Rule', 'Applying rule'),
+            new TracetoolTreeItem('Start transaction', 'Start transaction'),
+            new TracetoolTreeItem('Applying policy', 'Applying policy'),
+            new TracetoolTreeItem('Applying rule', 'Applying rule'),
             new TracetoolTreeItem('Query result', 'from policy result\\\\|Read result'), // escape regex by \\ and escape that to use in string by \\
             new TracetoolTreeItem('Subscriber result', 'Submitting document to subscriber shim')
         ];
